1ecf68578158afbd29c7e02fe18b34dacffe6cc9
Leblanc Sébastien <sugii.noob@gmail.com>
2025-11-25 16:08:24 +0100

    wip

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,6 +3,7 @@
   <div class="flex justify-center items-center h-screen">
     <h1 class="text-2xl">Page de test</h1>
   </div> -->
+  <div class="mt-4 rounded bg-blue-500 p-4 text-white hover:bg-blue-600"></div>
   <ColorTest />
 </template>
 
